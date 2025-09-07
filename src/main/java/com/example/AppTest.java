e0e252b9b98d5cfd3b1c1c55c21351808a99c0a6
Ayush <nayak.ap@somaiya.edu>
2025-09-07 17:24:37 +0530

    Initial Maven project for Jenkins

---
+++ src/main/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);  // dummy test
+    }
+
+    private void assertTrue(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assertTrue'");
+    }
+}
