e986b3d3d61208e1db97d9bd322bdca2005704f4
Ayush <nayak.ap@somaiya.edu>
2025-09-08 12:47:25 +0530

    New

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class App {
+    public static String hello() {
+        return "Hello, Jenkins!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! CI/CD pipeline is working!");
+        System.out.println(hello());
     }
 }
